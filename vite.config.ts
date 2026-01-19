163e97a9b46525c04b4b784f734096fc29a17743
seansonlin <seansonlin@gmail.com>
2026-01-19 15:37:44 +0800

    Fix: Update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/glasses-shop/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/glassshoping.github.io/' : '/',
 })
